73f1768ebbd879883f7d2b3964d2673529efbdbf
gavinnormand <normandgavin@gmail.com>
2025-06-07 21:28:17 -0700

    Removed resume page entirely, replaced by straight up file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
-import Projects from './pages/projects'
-import Experiences from './pages/experiences'
-import Skills from './pages/skills'
-import Nav from './components/nav'
-import Footer from './components/footer'
-import Photos from './pages/photos'
-import Resume from './pages/resume'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Projects from "./pages/projects";
+import Experiences from "./pages/experiences";
+import Skills from "./pages/skills";
+import Nav from "./components/nav";
+import Footer from "./components/footer";
+import Photos from "./pages/photos";
 
 function App() {
   return (
@@ -22,11 +21,10 @@ function App() {
         <Route path="/experiences" element={<Experiences />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/photos" element={<Photos />} />
-        <Route path="/resume" element={<Resume />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
